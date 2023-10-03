fd8b5fcb086b7beaa98fd35b9fb9a8dc05cb303a
chase <yicha7@gmail.com>
2023-10-03 15:21:14 +0900

    https

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const bodyParser = require('body-parser')
+const { default: axios } = require('axios')
 
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 const data = {
     load : ()=>{
@@ -23,12 +25,14 @@ const data = {
 }
 
 
-app.get('/', function (req, res) {
-    res.send(data.load())
+app.get('/', async function (req, res) {
+    let d = await axios('https://opendict.korean.go.kr/api/search?key=4E4101E1F9C6B578FCE4D6CABE483676&target_type=search&req_type=json&part=word&q=%EA%B5%AC%EB%91%90');
+    
+    res.send(d.data)
 })
 
 app.post('/insert', function (req, res) {
      res.send(data.save(req.body))
  })
 
-app.listen(3000)
+app.listen(3030)
